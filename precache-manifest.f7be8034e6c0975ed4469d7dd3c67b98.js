83d26e90bca522b233c0c09df91b2501d55f1a90
MCasante <math.casante@gmail.com>
2020-07-29 00:21:03 -0300

    Updates

---
+++ precache-manifest.f7be8034e6c0975ed4469d7dd3c67b98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d65437421d07b664285a5b6d17bc8c5",
+    "url": "/piximo/index.html"
+  },
+  {
+    "revision": "f922969b6999c81885ed",
+    "url": "/piximo/static/css/main.a3259cdf.chunk.css"
+  },
+  {
+    "revision": "bbd95fd03b04da5dd625",
+    "url": "/piximo/static/js/2.8b8eb8ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/piximo/static/js/2.8b8eb8ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f922969b6999c81885ed",
+    "url": "/piximo/static/js/main.590c8abd.chunk.js"
+  },
+  {
+    "revision": "9aadabec766d79cd7340",
+    "url": "/piximo/static/js/runtime-main.385c256b.js"
+  },
+  {
+    "revision": "5dee734632261b4b2bc4ebbda731f38b",
+    "url": "/piximo/static/media/logo.5dee7346.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9f4055ee90a0b1100570d0409e25db3e.js"
+  "/piximo/precache-manifest.f7be8034e6c0975ed4469d7dd3c67b98.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/piximo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
