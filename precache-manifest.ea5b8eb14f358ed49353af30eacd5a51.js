7034399cd00e2d4f5b49cfcd7e923a2a63b6e84c
MCasante <math.casante@gmail.com>
2020-07-29 18:39:15 -0300

    Updates

--- precache-manifest.ea5b8eb14f358ed49353af30eacd5a51.js
+++ precache-manifest.ea5b8eb14f358ed49353af30eacd5a51.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1e20e98ac7290808569a645c771929e0",
+    "revision": "4e1bebc968e537f084066fe2f9f2910a",
     "url": "/piximo/index.html"
   },
   {
-    "revision": "07e02fa1dff979b70586",
+    "revision": "aa4837e1f67ff090a3ee",
     "url": "/piximo/static/css/main.37c63db3.chunk.css"
   },
   {
-    "revision": "bbd95fd03b04da5dd625",
-    "url": "/piximo/static/js/2.8b8eb8ba.chunk.js"
+    "revision": "37cf0ea89503fa0fd9eb",
+    "url": "/piximo/static/js/2.20762c12.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/piximo/static/js/2.8b8eb8ba.chunk.js.LICENSE.txt"
+    "url": "/piximo/static/js/2.20762c12.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "07e02fa1dff979b70586",
-    "url": "/piximo/static/js/main.79ef6a06.chunk.js"
+    "revision": "aa4837e1f67ff090a3ee",
+    "url": "/piximo/static/js/main.18db9e63.chunk.js"
   },
   {
     "revision": "9aadabec766d79cd7340",
